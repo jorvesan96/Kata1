02feabbaf019e180f5f2e2b24d5bcf67eeed1f79
jorve <jorve@DESKTOP-5E2NNK5>
2021-09-23 12:16:15 +0100

    Kata1 versión3

--- src/kata1/pkg2021/Kata12021.java
+++ src/kata1/pkg2021/Kata12021.java
@@ -1,17 +1,16 @@
 
 package kata1.pkg2021;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
+
 
 
 public class Kata12021 {
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1949, 9, 24);
-        Person person = new Person("Blas", date);
-        System.out.println(person.getName()+" tiene "+person.getAge()+ " años.");
+        LocalDate birthday = LocalDate.of(1949,9,24);
+        Person person = new Person("Blas", birthday);
+        System.out.println(person.getName()+" tiene "+person.getAge().getYears()+ " años.");
     }
     
 }
--- src/kata1/pkg2021/Person.java
+++ src/kata1/pkg2021/Person.java
@@ -1,11 +1,13 @@
 package kata1.pkg2021;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
         private final String name;
-        private final Date birth;
-    public Person(String name, Date birth) {
+        private final LocalDate birth;
+        private final long MILISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
+    public Person(String name, LocalDate birth) {
         this.birth=birth; 
         this.name=name;
     }
@@ -14,12 +16,17 @@ public class Person {
         return name;
     }
 
-    public Date getBirth() {
+    public LocalDate getBirth() {
         return birth;
     }
     
-    public int getAge(){
-        return (int) ((new Date().getTime()-birth.getTime())/3153600000L);
+    public Period getAge(){
+        LocalDate today=LocalDate.now();
+        return  Period.between(birth, today);
+    }
+    
+    private long milliSecondsToYear(long milles){
+        return milles/MILISECONDS_PER_YEAR;
     }
     
 }
